38a19f537f45c36c385a2c4be8e9d58bd4c00c3c
kz2007 <kh.zgueb@gmail.com>
2022-11-05 14:47:41 +0100

    Third commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import MyProfile from './/profile/profile.js';
 
 const MyProfilePhoto = "https://tabib.ae/uploads/profile/free-profile-photo-whatsapp-4_1617693292_1625640135.png"
 const ImageSize = {  width: "360px", height: "360px"}
+const JamesProfile ={fullName: "James Robinson", bio: "A very good developer .", profession: "Developer"}
+
 
 const ProfileImage = () =>
 {
@@ -11,10 +13,12 @@ const ProfileImage = () =>
   );
 }
 function App() {
+
+
   const handleNameFunction  = name => alert(name);
   return (
     <>
-      <MyProfile fullName="James Robinson" bio="A very good developer ." profession="Developer" handleName ={handleNameFunction}>
+      <MyProfile fullName={JamesProfile.fullName} bio={JamesProfile.bio} profession={JamesProfile.profession} handleName ={handleNameFunction}>
         <ProfileImage />
       </MyProfile>
     </>
